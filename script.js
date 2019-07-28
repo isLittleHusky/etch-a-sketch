ee66c1efc3dec037e142115f8c2fefb7783a7757
Dylan de Kock <jddekock2001@gmail.com>
2019-07-28 18:37:03 +0200

    Fix gridCreate function returning width 0

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const container = document.querySelector('#container');
 
 function gridCreate(nSides) {
-	const length = container.style.width;
+	let width = container.getBoundingClientRect().width
+	const length = Math.floor(width/nSides);
 	var i;
 	for (i = 0; i < nSides**2; i++) {
 		let div = document.createElement('div');
-		console.log(div);
 		div.classList.add('content');
-		div.style.width = '' + length + 'px';
-		div.style.height = '' + length + 'px';
-		console.log(div.style.height);
-		container.appendChild(div)
+		div.setAttribute('style', 'width: ' + length + 'px; ' + ' height: ' + length + 'px')
+		container.appendChild(div);
 	}
 }
 
